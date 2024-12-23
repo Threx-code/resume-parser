387c44d232daf88aac11e75724fefd89b64b546b
Oluwatosin Amokeodo <oluwatosin.amokeodo@gmail.com>
2024-12-23 08:53:53 +0100

    Adding exceptions

--- main.py
+++ main.py
@@ -51,40 +51,5 @@ if __name__ == "__main__":
 
 
 
-    # salary_from = criteria.get("salary_from", "")
-    # salary_to = criteria.get("salary_to", "")
-    #
-    # query =  "-".join(criteria.get("keywords", []))
-    # location = criteria.get("location", "")
-    # experience = criteria.get("experience", "")
-    # salary = criteria.get("salary", "")
-    #
-    # url = f"https://www.work.ua/en/resumes-{criteria.get('location', [])}-"
-    #
-    # urls = f"{url}{query}/?experience={experience}&salary={salary}"
-    # print(urls)
-
-
-    # sr = ScrapperRequest(urls)
-    # soup = sr.get_soup()
-
-    # drt = soup.find_all("div", class_="resume-link")
-    # print(drt)
-    # resumes = []
-    # for resume in soup.select(".cv-item"):
-    #     resumes.append({
-    #         "name": resume.select_one(".cv-title").text.strip(),
-    #         "link": resume.select_one("a")['href'],
-    #         "details": resume.select_one(".cv-details").text.strip()
-    #     })
-    # print(resumes)
-
-
-
-
-
-
-
-
 
 
